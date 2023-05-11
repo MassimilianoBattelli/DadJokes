929aab0608ea51daa0d6f409c1c2a4015ad9e3df
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-05-11 20:57:14 +0200

    Retrofit

---
+++ app/src/main/java/com/example/dadjokes/remote/JokeRemoteService.kt
@@ -0,0 +1,6 @@
+package com.example.dadjokes.remote
+
+interface JokeRemoteService {
+
+
+}
---
+++ app/src/main/java/com/example/dadjokes/remote/RemoteApi.kt
@@ -0,0 +1,20 @@
+package com.example.dadjokes.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RemoteApi {
+
+    private const val BASE_URL = ""
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+}
