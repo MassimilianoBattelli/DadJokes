6e9b96d2ee2457259d4ad7b248969583214979ae
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-05-17 13:05:58 +0200

    Handle onBackPressed functionality

--- app/src/main/java/com/example/dadjokes/MainActivity.kt
+++ app/src/main/java/com/example/dadjokes/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dadjokes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,24 +15,65 @@ import com.example.dadjokes.ui.JokeAdapter
 import com.example.dadjokes.viewmodels.HomePageViewModelFactory
 import com.example.dadjokes.viewmodels.HomepageViewModel
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
-
+    private var currentFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            // Ripristina il fragment corrente
+            currentFragment = supportFragmentManager.getFragment(savedInstanceState, "currentFragment")
+        } else {
+            // Crea e visualizza il fragment iniziale
+            currentFragment = RecyclerFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_main, currentFragment!!)
+                .commit()
+        }
+
         val fragment = AppbarFragment()
         supportFragmentManager.beginTransaction()
             .replace(R.id.container_appbar, fragment)
             .commit()
-
-        val fragment2 = RecyclerFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container_main, fragment2)
-            .commit()
-
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Salva lo stato del fragment corrente
+        if (currentFragment != null) {
+            supportFragmentManager.putFragment(outState, "currentFragment", currentFragment!!)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentFragment = supportFragmentManager.getFragment(savedInstanceState, "currentFragment")
+        if (currentFragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_main, currentFragment!!)
+                .commit()
+        }
+    }
+
+
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager
+        val backStackEntryCount = fragmentManager.backStackEntryCount
+
+        if (backStackEntryCount > 0) {
+            fragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
+
+        val mainPageFragment = RecyclerFragment() // Replace RecyclerFragment with your actual main page fragment class
+        fragmentManager.beginTransaction()
+            .replace(R.id.container_main, mainPageFragment)
+            .commit()
+    }
+
 }
--- app/src/main/java/com/example/dadjokes/fragments/AppbarFragment.kt
+++ app/src/main/java/com/example/dadjokes/fragments/AppbarFragment.kt
@@ -37,5 +37,4 @@ class AppbarFragment : Fragment() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-
 }
